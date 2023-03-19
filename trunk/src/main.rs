40e3c8e79493948794f372eafe09afb11d1fe444
Tyler Holewinski <tyler.holewinski@bryx.com>
2023-03-19 11:21:04 -0400

    feat(trunk): add cors support

--- trunk/src/main.rs
+++ trunk/src/main.rs
@@ -3,10 +3,11 @@ use db::AspenDB;
 use lazy_static::lazy_static;
 
 use proto::{authorization_server::AuthorizationServer, links_server::LinksServer};
-use tonic::transport::Server;
+use tonic::{transport::Server, codegen::http::Method};
 
 use services::{authorization::AuthorizationService, links::LinksService};
-use tower_http::trace::TraceLayer;
+use tower::ServiceBuilder;
+use tower_http::{trace::TraceLayer, cors::{CorsLayer, Any}};
 
 pub mod proto {
   tonic::include_proto!("aspen.trunk");
@@ -33,6 +34,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
   Server::builder()
     .layer(TraceLayer::new_for_grpc())
+    .layer(CorsLayer::new().allow_origin(Any).allow_methods([Method::POST, Method::OPTIONS]))
     .add_service(AuthorizationServer::new(AuthorizationService::default()))
     .add_service(LinksServer::new(LinksService::default()))
     .serve(addr)
