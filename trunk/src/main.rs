ee00e2b7b8460a3abffb54c921db464396addc0e
Tyler Holewinski <tyler.holewinski@bryx.com>
2023-03-19 12:12:06 -0400

    feat(trunk): add logging and support for grpc-web

--- trunk/src/main.rs
+++ trunk/src/main.rs
@@ -3,19 +3,19 @@ use db::AspenDB;
 use lazy_static::lazy_static;
 
 use proto::{authorization_server::AuthorizationServer, links_server::LinksServer};
-use tonic::{transport::Server, codegen::http::Method};
+use tonic::transport::Server;
 
 use services::{authorization::AuthorizationService, links::LinksService};
-use tower::ServiceBuilder;
-use tower_http::{trace::TraceLayer, cors::{CorsLayer, Any}};
+use tower_http::trace::{DefaultMakeSpan, DefaultOnFailure, DefaultOnResponse, TraceLayer};
+use tracing::Level;
 
 pub mod proto {
   tonic::include_proto!("aspen.trunk");
 }
 
 mod db;
-mod jwt;
 mod helpers;
+mod jwt;
 
 mod services {
   pub mod authorization;
@@ -23,20 +23,32 @@ mod services {
 }
 
 lazy_static! {
-  pub static ref DB: AsyncOnce<AspenDB> = AsyncOnce::new(async {
-    AspenDB::init().await.unwrap()
-  });
+  pub static ref DB: AsyncOnce<AspenDB> = AsyncOnce::new(async { AspenDB::init().await.unwrap() });
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
   let addr = "0.0.0.0:9000".parse().unwrap();
 
+  tracing_subscriber::fmt().with_target(false).compact().init();
+
+  let auth_svc = tonic_web::config()
+    .allow_all_origins()
+    .enable(AuthorizationServer::new(AuthorizationService::default()));
+
+  let links_svc =
+    tonic_web::config().allow_all_origins().enable(LinksServer::new(LinksService::default()));
+
   Server::builder()
-    .layer(TraceLayer::new_for_grpc())
-    .layer(CorsLayer::new().allow_origin(Any).allow_methods([Method::POST, Method::OPTIONS]))
-    .add_service(AuthorizationServer::new(AuthorizationService::default()))
-    .add_service(LinksServer::new(LinksService::default()))
+    .accept_http1(true)
+    .layer(
+      TraceLayer::new_for_grpc()
+        .make_span_with(DefaultMakeSpan::new().level(Level::INFO))
+        .on_response(DefaultOnResponse::new().level(Level::INFO))
+        .on_failure(DefaultOnFailure::new().level(Level::INFO)),
+    )
+    .add_service(auth_svc)
+    .add_service(links_svc)
     .serve(addr)
     .await?;
 
