a1c45e0f67b43898b0b8aec119e97f9acc5c738f
Tyler Holewinski <tyler.holewinski@bryx.com>
2023-04-01 18:48:00 -0400

    feat(catkin): add basic tab support

--- catkin/catkin/ContentView.swift
+++ catkin/catkin/ContentView.swift
@@ -9,13 +9,27 @@ import SwiftUI
 
 struct ContentView: View {
     @AppStorage("jwt") var jwt: String?
+    @State private var curTabIdx = 0
     
     var body: some View {
         Group {
             if let authority = AspenAuthority.init(from: jwt) {
-                VStack {
+                TabView(selection: $curTabIdx) {
                     Text("Hello, \(authority.firstName)")
+                        .tabItem {
+                            Image(systemName: "links")
+                            Text("Links")
+                        }
+                        .tag(0)
+                    
+                    Text("Settings, \(authority.firstName)")
+                        .tabItem {
+                            Image(systemName: "1.circle")
+                            Text("Settings")
+                        }
+                        .tag(1)
                 }
+                .accentColor(Color(UIColor.blue))
                 .padding()
             } else {
                 LoginView() {
